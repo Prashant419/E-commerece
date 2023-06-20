90661939cd28c7822ba5ad1e0662108cf12e3948
Prashant Sharma <97109392+Prashant419@users.noreply.github.com>
2023-06-20 22:50:33 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,31 @@
+package com.example.ecommerce;
+
+import javax.xml.transform.Result;
+import java.sql.ResultSet;
+
+public class Login {
+    public Customer customerLogin(String userName, String password){
+        String loginQuery = "SELECT * FROM customer WHERE email = '"+userName+"' AND password = '"+password+"'";
+        DbConnection conn = new DbConnection();
+        ResultSet rs = conn.getQueryTable(loginQuery);
+        try{
+            if(rs.next()){
+                return new Customer(rs.getInt("id"), rs.getString("name"),
+                        rs.getString("email"), rs.getString("mobile"));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        Customer customer = login.customerLogin("prashant@gmail.com", "abc123");
+        System.out.println("Welcome: "+customer.getName());
+
+    }
+
+
+}
